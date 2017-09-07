75ec1a07e6d4cd3017c9ebe0b13bcdfae43a5faa
asherikov <asherikov@yandex.com>
2017-09-07 12:21:46 +0300

    added options_disablesolvers.cmake (to be used with non-public solvers)

---
+++ cmake/options_disablesolvers.cmake
@@ -0,0 +1,6 @@
+set(HUMOTO_ENABLE_MODULES_BY_DEFAULT    ON      CACHE BOOL "")
+set(HUMOTO_ENABLE_BRIDGES_BY_DEFAULT    ON      CACHE BOOL "")
+
+set(HUMOTO_BRIDGE_QuadProgpp            OFF     CACHE BOOL "")
+set(HUMOTO_BRIDGE_qpOASES               OFF     CACHE BOOL "")
+set(HUMOTO_BRIDGE_qpmad                 OFF     CACHE BOOL "")
