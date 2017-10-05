701b832e0b85a1b79066ae0fa685b5a06ab896a1
jmichalczyk <jasiekmi@wp.pl>
2017-10-05 09:50:07 +0200

    add ObstaclBase interface in obstacle_avoidance bridge

---
+++ bridges/obstacle_avoidance/obstacle_avoidance.h
@@ -0,0 +1,21 @@
+/**
+    @file
+    @author Jan Michalczyk
+    @copyright 2014-2017 INRIA. Licensed under the Apache License, Version 2.0.
+    (see @ref LICENSE or http://www.apache.org/licenses/LICENSE-2.0)
+    @brief
+*/
+
+#pragma once
+
+namespace humoto
+{
+    /**
+     * @brief Obstacle avoidance namespace.
+     */
+    namespace obstacle_avoidance
+    {
+    }
+}
+
+#include "obstacles/obstacle_base.h"
---
+++ bridges/obstacle_avoidance/obstacles/obstacle_base.h
@@ -0,0 +1,133 @@
+/**
+    @file
+    @author Jan Michalczyk
+    @copyright 2014-2017 INRIA. Licensed under the Apache License, Version 2.0.
+    (see @ref LICENSE or http://www.apache.org/licenses/LICENSE-2.0)
+    @brief
+*/
+
+#pragma once
+
+namespace humoto
+{
+    namespace obstacle_avoidance
+    {
+        /**
+         * @brief Abstract base class for obstacles.
+         */
+        class HUMOTO_LOCAL ObstacleBase : public humoto::rigidbody::RigidBodyState
+        {
+            public:
+                /**
+                 * @brief Constructor.
+                 *
+                 * @param[in] position
+                 * @param[in] rpy
+                 */
+                ObstacleBase(const etools::Vector3& position, 
+                             const etools::Vector3& rpy)
+                {
+                    updateState(position, rpy);
+                }
+                
+                
+                /**
+                 * @brief Constructor.
+                 */
+                ObstacleBase()
+                {
+                }
+                
+                
+                /**
+                 * @brief Update state.
+                 *
+                 * @param[in] obstacle_state
+                 */
+                void updateState(const humoto::rigidbody::RigidBodyState& obstacle_state)
+                {
+                    position_     = obstacle_state.position_;
+                    velocity_     = obstacle_state.velocity_;
+                    acceleration_ = obstacle_state.acceleration_;
+
+                    rpy_                  = obstacle_state.rpy_;
+                    angular_velocity_     = obstacle_state.angular_velocity_;
+                    angular_acceleration_ = obstacle_state.angular_acceleration_;
+                }
+                
+                
+                /**
+                 * @brief Update state.
+                 *
+                 * @param[in] position
+                 * @param[in] rpy
+                 */
+                void updateState(const etools::Vector3& position, 
+                                 const etools::Vector3& rpy)
+                {
+                    humoto::rigidbody::RigidBodyState obstacle_state;
+                    obstacle_state.position_ = position;
+                    obstacle_state.rpy_      = rpy;
+
+                    updateState(obstacle_state);
+                }
+                
+                
+                /**
+                 * @brief Get A matrix of linear constraints.
+                 *
+                 * @return    A matrix
+                 */
+                const Eigen::MatrixXd& getA() const
+                {
+                    return(A_);
+                }
+                
+                
+                /**
+                 * @brief Get bounds of linear constraints.
+                 *
+                 * @return    bounds
+                 */
+                const Eigen::VectorXd& getBounds() const 
+                {
+                    return(ub_);
+                }
+                
+                
+                /**
+                 * @brief Update linear constraints.
+                 *
+                 * @param[in] control_problem
+                 * @param[in] solution
+                 * @param[in] safety_margin
+                 */
+                virtual void updateConstraints(const humoto::ControlProblem& control_problem,
+                                               const humoto::Solution&       solution, 
+                                               const double                  safety_margin) = 0;
+                
+                
+                /**
+                 * @brief Initialize linear constraints.
+                 *
+                 * @param[in] control_problem
+                 */
+                virtual void initializeConstraints(const humoto::ControlProblem& control_problem)
+                {
+                }
+                
+                
+                /**
+                 * @brief Destructor.
+                 */
+                virtual ~ObstacleBase()
+                {
+                }
+
+
+            protected:
+                Eigen::MatrixXd A_;
+                Eigen::VectorXd ub_;
+        };
+    }
+}
